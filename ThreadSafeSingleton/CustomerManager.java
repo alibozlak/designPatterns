674b857f85f24d998db72229b61aa7e6b7648726
Ali Bozlak <ali-bozlak@outlook.com>
2022-10-17 21:52:46 +0300

    added synchronized keyword

--- ThreadSafeSingleton/CustomerManager.java
+++ ThreadSafeSingleton/CustomerManager.java
@@ -3,13 +3,12 @@ package ThreadSafeSingleton;
 class CustomerManager {
 
     private static CustomerManager customerManager;
-    static Object lockObject = new Object();
 
     private CustomerManager(){
 
     }
 
-    public static CustomerManager createAsSingleton() {
+    public static synchronized CustomerManager createAsSingleton() {
         if (customerManager == null) {
             customerManager = new CustomerManager();
         }
