1568d6ecfaa6d3d01ff033d3b660bf62d69b6ab5
Ali Bozlak <ali-bozlak@outlook.com>
2022-10-19 11:19:28 +0300

    Added comment line

--- FactoryMethodDesign/LoggerFactory.java
+++ FactoryMethodDesign/LoggerFactory.java
@@ -4,6 +4,7 @@ public class LoggerFactory implements ILoggerFactory {
 
     @Override
     public Logger createLogger() {
+        // *** Amacına göre Logger verilebilir. ***
         return new EdLogger();
     }
 }
